3dc224c638288df506e9b74c2cf8218e7a1cef17
judecima <juliodecima@gmail.com>
2026-09-26 15:10:29 -0300

    feat(furniture): export reusable 3D preview components

---
+++ src/components/furniture/index.ts
@@ -0,0 +1,6 @@
+export { FurniturePreview, type FurniturePreviewProps } from './furniture-preview';
+export {
+  FurnitureViewer,
+  type FurnitureViewerHandle,
+  type FurnitureViewerProps,
+} from './furniture-viewer';
